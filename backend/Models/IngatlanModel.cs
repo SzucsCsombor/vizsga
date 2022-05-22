e7eb12d8365687c285c75af6d3e7629681f9cafb
SzucsCsombor <90245911+SzucsCsombor@users.noreply.github.com>
2022-05-22 18:32:58 +0200

    Add files via upload

---
+++ backend/Models/IngatlanModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ingatlan.Models
+{
+    [Table("ingatlanok")]
+    public class IngatlanModel
+    {
+        public int Id { get; set; }
+        public KategoriaModel Kategoria { get; set; }
+        public string Leiras { get; set; }
+        public DateTime HirdetesDatuma { get; set; } = DateTime.Today;
+        public bool Tehermentes { get; set; }
+        public uint Ar { get; set; }
+        public string KepUrl { get; set; }
+
+        public int KategoriaId { get; set; }
+    }
+}
---
+++ backend/Models/KategoriaModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ingatlan.Models
+{
+    [Table("kategoriak")]
+    public class KategoriaModel
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Nev { get; set; }
+    }
+}
